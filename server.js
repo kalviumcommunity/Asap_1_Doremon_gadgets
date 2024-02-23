2014f2826cf321f396ab47c55d65437a68c03ac1
pranjal-2507 <144115844+pranjal-2507@users.noreply.github.com>
2024-02-23 17:37:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const port = process.env.PUBLIC_PORT;
 
 // define the ping route
 app.get("/", (req,res)=>{
-  res.send('Hello, this is the default route page')
+  res.json({
+    message: 'o_O',
+    database: isConnected() ? 'connected' : 'disconnected'
+  });
 })
 app.get("/ping",(req,res)=>{
   res.send('pong')
