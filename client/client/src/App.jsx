fbe8ac0e738a50a3a8702695040eabd0a1f46ca6
Pranjal <pranjalgosavi2507@gmail.com>
2024-02-28 10:35:30 +0530

    Created the landing page

---
+++ client/client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Body from './Components/Body'
+import Navbar from './Components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar/>
+      <Body/>
+       
+    </>
+  )
+}
+
+export default App
